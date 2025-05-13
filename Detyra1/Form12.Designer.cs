c9c0da6ace5412131463d0c06d9690eadd6be002
leoneberisha <leonaberiisha@gmail.com>
2025-05-13 19:01:52 +0200

    .

--- Detyra1/Form12.Designer.cs
+++ Detyra1/Form12.Designer.cs
@@ -65,7 +65,7 @@
             this.dataGridView1.Name = "dataGridView1";
             this.dataGridView1.RowHeadersWidth = 51;
             this.dataGridView1.RowTemplate.Height = 24;
-            this.dataGridView1.Size = new System.Drawing.Size(1065, 503);
+            this.dataGridView1.Size = new System.Drawing.Size(1066, 467);
             this.dataGridView1.TabIndex = 0;
             // 
             // Column1
@@ -199,7 +199,7 @@
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
-            this.ClientSize = new System.Drawing.Size(1064, 694);
+            this.ClientSize = new System.Drawing.Size(1061, 701);
             this.Controls.Add(this.textBox1);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.button5);
