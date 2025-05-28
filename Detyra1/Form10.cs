f06ad790240a2938e64407f969c14813e22f0fd1
leoneberisha <leonaberiisha@gmail.com>
2025-05-28 02:40:59 +0200

    .

--- Detyra1/Form10.cs
+++ Detyra1/Form10.cs
@@ -1,14 +1,7 @@
 ﻿using MySql.Data.MySqlClient;
 using System;
-using System.Collections.Generic;
-using System.ComponentModel;
 using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
-
 
 namespace Detyra1
 {
@@ -22,22 +15,26 @@ namespace Detyra1
             LoadBuzekuq();
         }
 
-
         public void LoadBuzekuq()
         {
-                      using (MySqlConnection conn = new MySqlConnection(connectionString))
+            using (MySqlConnection conn = new MySqlConnection(connectionString))
             {
-                string query = "SELECT id, emriFurnitorit as 'Emri Furnitorit', emriBuzekuqit as 'Emri Buzekuqit', nuanca as 'Nuanca', sasia as 'Sasia', cmimiBlerjes as 'Cmimi Blerjes', cmimiShitjes as 'Cmimi Shitjes', totali as 'Totali' FROM buzekuq";
+                string query = "SELECT id, emriFurnitorit AS 'Emri Furnitorit', emriBuzekuqit AS 'Emri Buzekuqit', " +
+                               "nuanca AS 'Nuanca', sasia AS 'Sasia', cmimiBlerjes AS 'Cmimi Blerjes', " +
+                               "cmimiShitjes AS 'Cmimi Shitjes', totali AS 'Totali' FROM buzekuq";
                 MySqlDataAdapter adapter = new MySqlDataAdapter(query, conn);
                 DataTable table = new DataTable();
                 adapter.Fill(table);
                 dataGridView1.DataSource = table;
+                dataGridView1.AllowUserToAddRows = false;
             }
         }
-         private void button1_Click(object sender, EventArgs e)
+
+        private void button1_Click(object sender, EventArgs e)
         {
             this.Close();
         }
+
         private void button3_Click(object sender, EventArgs e)
         {
             Form9 form9 = new Form9(this);
@@ -49,97 +46,78 @@ namespace Detyra1
             if (dataGridView1.SelectedRows.Count > 0)
             {
                 DataGridViewRow selectedRow = dataGridView1.SelectedRows[0];
+
                 int id = Convert.ToInt32(selectedRow.Cells["id"].Value);
-                string emriFurnitorit = selectedRow.Cells["emriFurnitorit"].Value.ToString();
+                string emriFurnitorit = selectedRow.Cells["Emri Furnitorit"].Value.ToString();
                 string emriBuzekuqit = selectedRow.Cells["Emri Buzekuqit"].Value.ToString();
-                string nuanca = selectedRow.Cells["nuanca"].Value.ToString();
-                string sasia = selectedRow.Cells["sasia"].Value.ToString();
-                string cmimiBlerjes = selectedRow.Cells["cmimiBlerjes"].Value.ToString();
-                string cmimiShitjes = selectedRow.Cells["cmimiShitjes"].Value.ToString();
-                string totali = selectedRow.Cells["totali"].ToString();
+                string nuanca = selectedRow.Cells["Nuanca"].Value.ToString();
+                string sasia = selectedRow.Cells["Sasia"].Value.ToString();
+                string cmimiBlerjes = selectedRow.Cells["Cmimi Blerjes"].Value.ToString();
+                string cmimiShitjes = selectedRow.Cells["Cmimi Shitjes"].Value.ToString();
+                string totali = selectedRow.Cells["Totali"].ToString();
 
                 Form9 form9 = new Form9(this, id, emriFurnitorit, emriBuzekuqit, nuanca, sasia, cmimiBlerjes, cmimiShitjes, totali, true);
                 form9.ShowDialog();
             }
             else
             {
-                MessageBox.Show("Zgjedh nje rrjesht per te edituar. ");
+                MessageBox.Show("Zgjedh një rresht për të edituar.");
             }
-            }
-
-
+        }
 
         private void button5_Click(object sender, EventArgs e)
         {
             if (dataGridView1.SelectedRows.Count > 0)
             {
-                DialogResult result = MessageBox.Show("A jeni i sigurt që dëshironi të fshini këtë fondatine?", "Konfirmo Fshirjen", MessageBoxButtons.YesNo, MessageBoxIcon.Warning);
+                DialogResult result = MessageBox.Show("A jeni i sigurt që dëshironi të fshini këtë produkt?", "Konfirmo Fshirjen", MessageBoxButtons.YesNo, MessageBoxIcon.Warning);
                 if (result == DialogResult.Yes)
                 {
                     int id = Convert.ToInt32(dataGridView1.SelectedRows[0].Cells["id"].Value);
-
-                    string connectionString = "server=localhost;database=sephorasistem;uid=root;pwd=;";
                     using (MySqlConnection conn = new MySqlConnection(connectionString))
                     {
-
-                        string query = "DELETE FROM buzekuq WHERE is=@id";
+                        string query = "DELETE FROM buzekuq WHERE id=@id";
                         MySqlCommand cmd = new MySqlCommand(query, conn);
                         cmd.Parameters.AddWithValue("@id", id);
 
                         conn.Open();
                         cmd.ExecuteNonQuery();
                         conn.Close();
-
                     }
+
                     LoadBuzekuq();
                 }
             }
             else
             {
-                MessageBox.Show("Zgjedh nje rresht qe deshiron ta fshish. ");
-            }
-        }
-                    
-
-        private void button5_Click_1(object sender, EventArgs e)
-        {
-            if (dataGridView1.SelectedRows.Count > 0)
-            {
-                DialogResult result = MessageBox.Show("A jeni i sigurt që dëshironi të fshini këtë fondatine?", "Konfirmo Fshirjen", MessageBoxButtons.YesNo, MessageBoxIcon.Warning);
-                if (result == DialogResult.Yes)
-                {
-                    int id = Convert.ToInt32(dataGridView1.SelectedRows[0].Cells["id"].Value);
-
-                    string connectionString = "server=localhost;database=sephorasistem;uid=root;pwd=;";
-                    using (MySqlConnection conn = new MySqlConnection(connectionString))
-                    {
-                        string query = "DELETE FROM buzekuq Where id = @id";
-                        MySqlCommand cmd = new MySqlCommand(query, conn);
-                        cmd.Parameters.AddWithValue("@id", id);
-
-                        conn.Open();
-                        cmd.ExecuteNonQuery();
-                        conn.Close();
-                    }
-
-                    LoadBuzekuq();
-                }
+                MessageBox.Show("Zgjedh një rresht për ta fshirë.");
             }
         }
 
         private void button2_Click(object sender, EventArgs e)
         {
-            string connectionString = "server=localhost;database=sephorasistem;uid=root;pwd=;";
             using (MySqlConnection conn = new MySqlConnection(connectionString))
             {
-                string query = "SELECT id, emriFurnitorit as 'Emri Furnitorit', emriBuzekuqit as 'Emri Buzekuqit', nuanca as 'Nuanca', sasia as 'Sasia', cmimiBlerjes as 'Cmimi Blerjes', cmimiShitjes as 'Cmimi Shitjes', totali as 'Totali' FROM buzekuq";
+                string query = @"SELECT id,
+                                emriFurnitorit AS 'Emri Furnitorit',
+                                emriBuzekuqit AS 'Emri Buzekuqit',
+                                nuanca AS 'Nuanca',
+                                sasia AS 'Sasia',
+                                cmimiBlerjes AS 'Cmimi Blerjes',
+                                cmimiShitjes AS 'Cmimi Shitjes',
+                                totali AS 'Totali'
+                         FROM buzekuq
+                         WHERE emriBuzekuqit LIKE @search";
+
                 MySqlCommand cmd = new MySqlCommand(query, conn);
                 cmd.Parameters.AddWithValue("@search", "%" + textBox1.Text + "%");
+
                 MySqlDataAdapter adapter = new MySqlDataAdapter(cmd);
                 DataTable table = new DataTable();
                 adapter.Fill(table);
                 dataGridView1.DataSource = table;
+                dataGridView1.AllowUserToAddRows = false;
             }
         }
+
     }
 }
