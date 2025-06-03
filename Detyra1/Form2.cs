d60ee551acab125197e3412f3cada76e4784e5ad
leoneberisha <leonaberiisha@gmail.com>
2025-06-03 16:20:43 +0200

    Update Form2.cs

--- Detyra1/Form2.cs
+++ Detyra1/Form2.cs
@@ -67,5 +67,13 @@ namespace Detyra1
         {
 
         }
+
+     
+        private void raportiDitorToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Form17 form17 = new Form17();
+            form17.ShowDialog();
+
+        }
     }
 }
