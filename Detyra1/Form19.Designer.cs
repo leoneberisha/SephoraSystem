bab9513be5f549721bb8bdec1f2187c6bf388d72
leoneberisha <leonaberiisha@gmail.com>
2025-09-12 23:25:57 +0200

    .

--- Detyra1/Form19.Designer.cs
+++ Detyra1/Form19.Designer.cs
@@ -28,6 +28,7 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form19));
             this.label1 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
             this.label3 = new System.Windows.Forms.Label();
@@ -261,6 +262,7 @@
             this.Controls.Add(this.label3);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.Name = "Form19";
             this.Text = "Fatura";
             this.Load += new System.EventHandler(this.Form19_Load);
